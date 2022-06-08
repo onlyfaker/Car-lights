03d278830e506dcf0f899c9e701f1b6c61da535a
AG <gemo.gemaljevic@gmail.com>
2022-06-08 11:19:12 +0200

    Create svetla.py

---
+++ svetla.py
@@ -0,0 +1,2 @@
+#make light turn on whne clicked on a particular key on a keyboard
+#klik na odredjeno dugme pali se zmigavas i posle nekog vremena se gasi, stop svetla se plai samo kad se drzi key, ukljucena prednja svetla sve dok se ne izgase
