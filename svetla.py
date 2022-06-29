730c1ae2a54b8117533ae56b7613095669e2369c
AG <gemo.gemaljevic@gmail.com>
2022-06-29 15:31:50 +0200

    Update svetla.py

--- svetla.py
+++ svetla.py
@@ -5,7 +5,7 @@ wn = turtle.Screen()
 wn.bgcolor('black')
 wn.title('carlights')
 
-#drwing a box for front lights
+#drwing a box for front left light
 penleft = turtle.Turtle()
 penleft.color('white')
 penleft.width(3)
@@ -21,33 +21,47 @@ penleft.fd(100)
 penleft.rt(90)
 penleft.fd(50)
 
-#drwaing a white light
+#drwaing a white front left light
 whiteleft = turtle.Turtle()
 whiteleft.shape('square')
 whiteleft.color('grey')
 whiteleft.shapesize(2.3,3.3,)
 whiteleft.goto(-235,175)
 
-#drawing a turning front light
-yellow = turtle.Turtle()
-yellow.shape('square')
-yellow.color('grey')
-yellow.shapesize(2.3,1.5,)
-yellow.goto(-284,175)
+#drawing a turning front left light
+yellowleft = turtle.Turtle()
+yellowleft.shape('square')
+yellowleft.color('grey')
+yellowleft.shapesize(2.3,1.5,)
+yellowleft.goto(-284,175)
 
-# #drwing second box for front lights
-# penright = turtle.Turtle()
-# penright.color('white')
-# penright.width(3)
-# penright.hideturtle()
-# penright.penup()
-# penright.goto(200,200)
-# penright.pendown()
-# penright.fd(100)
-# penright.rt(90)
-# penright.fd(50)
-# penright.rt(90)
-# penright.fd(100)
-# penright.rt(90)
-# penright.fd(50)
+#drwing a box for front right light
+penright = turtle.Turtle()
+penright.color('white')
+penright.width(3)
+penright.hideturtle()
+penright.penup()
+penright.goto(200,200)
+penright.pendown()
+penright.fd(100)
+penright.rt(90)
+penright.fd(50)
+penright.rt(90)
+penright.fd(100)
+penright.rt(90)
+penright.fd(50)
+
+#drwaing a white front right light
+whiteright = turtle.Turtle()
+whiteright.shape('square')
+whiteright.color('grey')
+whiteright.shapesize(2.3,3.3,)
+whiteright.goto(235,175)
+
+#drawing a turning front left light
+yellowright = turtle.Turtle()
+yellowright.shape('square')
+yellowright.color('grey')
+yellowright.shapesize(2.3,1.5,)
+yellowright.goto(284,175)
 
